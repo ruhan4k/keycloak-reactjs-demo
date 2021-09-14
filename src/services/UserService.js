1a29db831c57d9f3187c406e7bdafad881d5b31b
Ruhan Bidart <ruhanbidart@gmail.com>
2021-09-14 17:00:19 -0300

    Change it to be used by the Fourkites DEV SS env

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 import Keycloak from "keycloak-js";
 
-const _kc = new Keycloak('/keycloak.json');
+const _kc = new Keycloak("/keycloak.json");
 
 /**
  * Initializes Keycloak instance and calls the provided callback function if successfully authenticated.
@@ -8,18 +8,20 @@ const _kc = new Keycloak('/keycloak.json');
  * @param onAuthenticatedCallback
  */
 const initKeycloak = (onAuthenticatedCallback) => {
-  _kc.init({
-    onLoad: 'check-sso',
-    silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
-    pkceMethod: 'S256',
-  })
-    .then((authenticated) => {
-      // if (authenticated) {
-      onAuthenticatedCallback();
-      // } else {
-      //   doLogin();
-      // }
+  _kc
+    .init({
+      onLoad: "login-required",
     })
+    .then((authenticated) => {
+      debugger;
+      console.log(_kc.token);
+      console.log(_kc.refreshToken);
+      if (authenticated) {
+        onAuthenticatedCallback();
+      } else {
+        doLogin();
+      }
+    });
 };
 
 const doLogin = _kc.login;
@@ -31,9 +33,7 @@ const getToken = () => _kc.token;
 const isLoggedIn = () => !!_kc.token;
 
 const updateToken = (successCallback) =>
-  _kc.updateToken(5)
-    .then(successCallback)
-    .catch(doLogin);
+  _kc.updateToken(5).then(successCallback).catch(doLogin);
 
 const getUsername = () => _kc.tokenParsed?.preferred_username;
 
